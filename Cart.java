6a1089835b6f389c59e045be93a2ee9430a7a32c
Althea Gonzales <altheagonzalesctr@gmail.com>
2024-10-24 12:40:13 +0800

    Create Cart.java

---
+++ Cart.java
@@ -0,0 +1,9 @@
+public class Cart implements HotelService {
+    public void requestCart(int numberOfCarts) {
+        System.out.println("Cart: Requesting " + numberOfCarts + " luggage cart(s).");
+    }
+
+    @Override
+    public void serviceMethod() {
+    }
+}
